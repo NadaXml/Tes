4dcea3921662b694a36f5426eff98fe6eb74c22e
DESKTOP-DM584UI\luxuming <nada_havok@163.com>
2021-04-12 01:42:20 +0800

    34

--- CustomExportBlock.cs
+++ CustomExportBlock.cs
@@ -11,7 +11,7 @@ namespace SLua
 
     public class CustomExportBlockEditor : EditorWindow
     {
-        [MenuItem("SLua/Unity/BlockWhite")]
+        [MenuItem("SLua/Custom/BlockWhite")]
         static public void showWnd()
         {
             CustomExportBlockEditor wnd = EditorWindow.GetWindow<CustomExportBlockEditor>("CustomExportBlockEditor");
@@ -25,71 +25,61 @@ namespace SLua
         void OnGUI()
         {
             GUILayout.BeginVertical();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("className");
-            className = GUILayout.TextField(className, GUILayout.Width(200));
-
-            if (GUILayout.Button("读取"))
             {
-                CustomExportBlockTool d = new CustomExportBlockTool();
-                d.loadJson();
-                Type tt = Type.GetType(className);
-                CustomExportBlock b = d.isValidClass(tt.Name);
-                if ( b != null )
+                GUILayout.BeginHorizontal();
                 {
-                    ConstructorInfo[] cInfos = tt.GetConstructors();
-                    foreach ( var cc in cInfos)
-                    {
-                        bool isValid = d.isValidConstructor(b, cc);
-                        Debug.Log(" dd  " + isValid);
-                    }
+                    GUILayout.Label("className");
+                    className = GUILayout.TextField(className, GUILayout.Width(200));
                 }
-            }
-            if ( GUILayout.Button("查看") )
-            {
-                paramList.Clear();
+                GUILayout.EndHorizontal();
 
-                Type tt = Type.GetType(className);
-
-                var r = tt;
-                Debug.Log(r.Name);
-                ConstructorInfo[] s = r.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
-
-                foreach (var info in s)
+                GUILayout.BeginHorizontal();
                 {
-                    paramList.Add("--------------------------");
-                    ParameterInfo[] pInfos = info.GetParameters();
-                    if (pInfos.Length > 0)
+                    if (GUILayout.Button("查看"))
                     {
-                        foreach (var pInfo in pInfos)
+                        paramList.Clear();
+
+                        Type tt = Type.GetType(className);
+
+                        var r = tt;
+                        Debug.Log(r.Name);
+                        ConstructorInfo[] s = r.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+
+                        foreach (var info in s)
                         {
-                            Debug.Log(pInfo.ParameterType.Name);
-                            paramList.Add(pInfo.ParameterType.Name);
+                            paramList.Add("--------------------------");
+                            ParameterInfo[] pInfos = info.GetParameters();
+                            if (pInfos.Length > 0)
+                            {
+                                foreach (var pInfo in pInfos)
+                                {
+                                    paramList.Add(pInfo.ParameterType.Name);
+                                }
+                            }
+                            else
+                            {
+                                paramList.Add("empty");
+                            }
                         }
                     }
-                    else
-                    {
-                        paramList.Add("empty");
-                    }
                 }
+                GUILayout.EndHorizontal();
+
+                scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.ExpandHeight(true));
+
+                foreach (var str in paramList)
+                {
+                    EditorGUILayout.SelectableLabel(str);
+                }
+                EditorGUILayout.EndScrollView();
             }
-
-            GUILayout.EndHorizontal();
-
-            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.ExpandHeight(true));
-            
-            foreach( var str in paramList)
-            {
-                EditorGUILayout.SelectableLabel(str);
-            }
-            EditorGUILayout.EndScrollView();
-
-
             GUILayout.EndVertical();
         }
     }
 
+    /// <summary>
+    /// 白名单json对应的类
+    /// </summary>
     [System.Serializable]
     public class CustomExportBlock
     {
@@ -112,12 +102,37 @@ namespace SLua
 
     public class CustomExportBlockTool
     {
+        static private CustomExportBlockTool instance;
+
+        static public CustomExportBlockTool getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CustomExportBlockTool();
+            }
+            return instance;
+        }
+        /// <summary>
+        /// Json读出的白名单数据
+        /// </summary>
         CustomExportBlocks datas;
 
-        Dictionary<string, CustomExportBlock> blockMap = new Dictionary<string, CustomExportBlock>();
+        /// <summary>
+        /// 类名白名单Map
+        /// </summary>
+        Dictionary<string, CustomExportBlock> blockMap;
         public void loadJson()
         {
-            string jsonStr = File.ReadAllText(Application.dataPath + "/test.json");
+            if (blockMap != null )
+            {
+                blockMap.Clear();
+            }
+            else
+            {
+                blockMap = new Dictionary<string, CustomExportBlock>();
+            }
+
+            string jsonStr = File.ReadAllText(Application.dataPath + "/Slua/Editor/test.json");
 
             datas = JsonUtility.FromJson<CustomExportBlocks>(jsonStr);
 
@@ -125,46 +140,74 @@ namespace SLua
             {
                 blockMap[block.className] = block;
             }
-
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="cInfo"></param>
+        /// <returns></returns>
+        public bool isValidConstructorInfo(string className, ConstructorInfo cInfo)
+        {
+            CustomExportBlock block = isValidClass(className);
+            return isValidConstructor(block, cInfo);
+        }
+            
+        /// <summary>
+        /// 需要检查构造函数的类名
+        /// </summary>
+        /// <param name="className"></param>
+        /// <returns></returns>
         public CustomExportBlock isValidClass(string className)
         {
+            if (blockMap == null )
+            {
+                return null;
+            }
             CustomExportBlock block = null;
             blockMap.TryGetValue(className, out block);
             return block;
         }
 
+        /// <summary>
+        /// 反射出来的合法构造函数
+        /// </summary>
+        /// <param name="block"></param>
+        /// <param name="cInfo"></param>
+        /// <returns></returns>
         public bool isValidConstructor(CustomExportBlock block, ConstructorInfo cInfo)
         {
             bool ret = true;
+            if(block == null || cInfo == null )
+            {
+                return ret;
+            }
             
             ParameterInfo[] pi = cInfo.GetParameters();
-            int paramSuit = 0;
-            foreach (var construct in block.constructor)
+            foreach (CustomExportBlock.BlockConstructor construct in block.constructor)
             {
+                int fitCount = 0;
                 if (pi.Length == construct.param.Count)
                 {
-
                     for (int i = 0; i < pi.Length; i++)
                     {
                         if (pi[i].ParameterType.Name.Equals(construct.param[i]))
                         {
-                            paramSuit++;
-                            break;
+                            fitCount++;
                         }
                     }
                 }
                 else
                 {
-                    paramSuit = -1;
+                    fitCount = -1;
                 }
-            }
-            if (paramSuit == pi.Length)
-            {
-                ret = false;
-            }
-                
+
+                if ( fitCount == pi.Length)
+                {
+                    ret = false;
+                }
+            }                
             return ret;
         }
     }
